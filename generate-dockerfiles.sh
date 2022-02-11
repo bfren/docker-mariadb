93d7d5892b2d11e5e5a25373ddf9382d3a7ca600
Ben Green <ben@bcgdesign.com>
2022-02-11 15:22:53 +0000

    Using latest base images

--- generate-dockerfiles.sh
+++ generate-dockerfiles.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 docker pull bfren/alpine
 
-BASE_REVISION="3.2.3"
+BASE_REVISION="4.0.4"
 echo "Base: ${BASE_REVISION}"
 
 MARIADB_VERSIONS="10.4 10.5 10.6"
@@ -15,6 +15,7 @@ for V in ${MARIADB_VERSIONS} ; do
 
     DOCKERFILE=$(docker run \
         -v ${PWD}:/ws \
+        -e BF_DEBUG=0 \
         bfren/alpine esh \
         "/ws/Dockerfile.esh" \
         BASE_REVISION=${BASE_REVISION} \
