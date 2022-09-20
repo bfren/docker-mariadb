5bf722f4b57f53abbfa00b0552f92d8e81a2f262
bfren <bf@bfren.dev>
2022-09-20 13:10:58 +0100

    v5.1.0 (#109)
    Major updates
    * Using latest base images (with full busybox)
    * Decompress backup files in import
    
    Minor updates
    * Updating to MariaDB 10.6.10, 10.7.7, 10.8.5, and 10.9.3
    * Removing broken if database exists check
    
    Build updates
    * Using Debian repo names

--- generate.sh
+++ generate.sh
@@ -4,14 +4,13 @@ set -euo pipefail
 
 docker pull bfren/alpine
 
-BASE_REVISION="1.0.4"
+BASE_REVISION="1.1.0"
 echo "Base: ${BASE_REVISION}"
 
 MARIADB_VERSIONS="10.4 10.5 10.6 10.7 10.8 10.9"
 for V in ${MARIADB_VERSIONS} ; do
 
     echo "MariaDB ${V}"
-    DEBIAN_NAME=`cat ./${V}/DEBIAN_NAME`
     DEBIAN_VERSION=`cat ./${V}/DEBIAN_VERSION`
 
     DOCKERFILE=$(docker run \
@@ -31,7 +30,7 @@ for V in ${MARIADB_VERSIONS} ; do
         -e BF_DEBUG=0 \
         bfren/alpine esh \
         "/ws/mariadb.list.esh" \
-        DEBIAN_NAME=${DEBIAN_NAME} \
+        DEBIAN_VERSION=${DEBIAN_VERSION} \
         MARIADB_MINOR=${V}
     )
 
