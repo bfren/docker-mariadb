74460b710a3d4b016f7ce5fb7a4f51f89358a1a9
bfren <bf@bfren.dev>
2023-02-13 09:05:16 +0000

    Using latest base images

--- generate.sh
+++ generate.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 docker pull bfren/alpine
 
-BASE_REVISION="1.1.13"
+BASE_REVISION="1.1.14"
 echo "Base: ${BASE_REVISION}"
 
 MARIADB_VERSIONS="10.4 10.5 10.6 10.7 10.8 10.9 10.10"
