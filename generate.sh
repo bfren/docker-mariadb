b5778abe8f3b4e8ae9a670c7dfd605913cccf38a
bfren <bf@bfren.dev>
2024-02-23 20:19:22 +0000

    Using latest Alpine base images

--- generate.sh
+++ generate.sh
@@ -5,11 +5,10 @@ set -euo pipefail
 docker pull bfren/alpine
 
 DEBIAN_BASE_REVISION="2.1.0"
-ALPINE_BASE_REVISION="5.2.0"
+ALPINE_BASE_REVISION="5.2.2"
 
 echo "Debian Base: ${DEBIAN_BASE_REVISION}"
 MARIADB_VERSIONS="10.4 10.5 10.6 10.11 11.0 11.1 11.2 11.3"
-#MARIADB_VERSIONS="11.2"
 for V in ${MARIADB_VERSIONS} ; do
 
     echo "MariaDB ${V}"
