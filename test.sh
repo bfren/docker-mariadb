fd02d59395bf3b65e319b5918f64cc0ef0be196e
bfren <bf@bfren.dev>
2024-02-23 20:15:17 +0000

    Adding new test framework

---
+++ test.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+IMAGE=mariadb
+VERSION=`cat VERSION`
+MARIADB=${1:-11.2}
+TAG=${IMAGE}-test
+
+docker buildx build \
+    --load \
+    --build-arg BF_IMAGE=${IMAGE} \
+    --build-arg BF_VERSION=${VERSION} \
+    -f ${MARIADB}/Dockerfile \
+    -t ${TAG} \
+    . \
+    && \
+    docker run --entrypoint "/usr/bin/env" ${TAG} -i nu -c "use bf test ; test"
